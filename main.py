7fea77fcc87213e03d5f6cd83c3027fc5714a215
San Sequeira <42314542+sansqra@users.noreply.github.com>
2021-11-16 10:50:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,11 @@ database = 'classicmodels'
 with open('file.sql','w') as output:
     c = subprocess.Popen(['mysqldump', '-u',username,'-p%s'%password,database],
                          stdout=output, shell=True)
+    
+ '''
+ add to the start of dump file
+ 
+ CREATE DATABASE /*!32312 IF NOT EXISTS*/`file` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-# from subprocess import Popen, PIPE
-
-# filename = 'C:\\Users\\Admin\\Desktop\\securityp8-msccs1\\file.sql'
-# process = Popen(['mysql', database, '-u', username, '-p', password],
-#                 stdout=PIPE, stdin=PIPE)
-# output = process.communicate(str.encode('source ' + filename))[0]
-# # output = process.run('mysql'))[0]
-
-# print(output)
-
-# import os
-# import datetime
-# from zipfile import ZipFile
-
-
-# BACKUP_DIR_NAME = "mysql_backups"
-# FILE_PREFIX = "my_db_backup_"
-# FILE_SUFFIX_DATE_FORMAT = "%Y%m%d%H%M%S"
-# USERNAME = "root"
-# DBNAME = USERNAME+"$mysqlsampledatabase.sql"
-
-
-# # get today's date and time
-# timestamp = datetime.datetime.now().strftime(FILE_SUFFIX_DATE_FORMAT)
-# backup_filename = BACKUP_DIR_NAME+"/"+FILE_PREFIX+timestamp+".sql"
-
-# os.system("mysqldump -u "+ password + ' ' + DBNAME+"'  > " + backup_filename)
+USE `file`;
+'''
